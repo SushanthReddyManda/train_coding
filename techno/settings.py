3aff9dffe1bb35b63ac3482d3a8bc47fa44ffd8e
KANDHVE950 <kandhve@iitg.ac.in>
2021-09-04 10:33:59 +0530

    Deleted db and migrations dir

--- techno/settings.py
+++ techno/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sw$pz&s)w0d=ejc!@gzmp^8+y5mm7#8fvi!022!plooxcl3w8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
